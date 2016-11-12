6e516cb83a8ed387b1c2b3c076471d5aa5b643d1
wompshmack <wompshmack@Eugene>
2016-11-12 12:46:30 -0600

    started writing classes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <SFML/Graphics.hpp>
 
+class Entity {
+  int hp;
+  int currenthp;
+  int strength;
+  int dexterity;
+  int luck;
+  int intelligence;
+  int movespeed;
+  int attackspeed;
+  //sprite placeholder
+public:
+  void update()
+  {
+    //state update(on the ground? etc)
+    //move by speed
+    //update sprite location
+  }
+  void damage(int)//probably float later
+  {
+    //hitstun
+    //reduce hp by damage ammount
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Bunny Killer: Origins");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -14,7 +36,7 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+	shape.move(0,0.1);
         window.clear();
         window.draw(shape);
         window.display();
